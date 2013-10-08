c230473da83ddd571a2059ac80953b40ab2b7f5f
Unknow <unknow0@wanadoo.fr>
2013-10-08 23:22:55 +0200

    added licensing notice & hashtable

--- container/hashmap.h
+++ container/hashmap.h
@@ -15,35 +15,50 @@
  * with libcontainer; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ******************************************************************************/
-#ifndef _LINKNED_LIST_H
-#define _LINKNED_LIST_H
+#ifndef _HASHMAP_H
+#define _HASHMAP_H
 
 #include <stdlib.h>
 #include <container/iterator.h>
 
-typedef struct list_node
-	{
-	struct list_node *next;
-	struct list_node *prev;
-	void *data;
-	} linked_node_t;
-
 typedef struct
 	{
-	size_t elem_size;
-
-	struct list_node *head;
-	struct list_node *tail;
-	} linked_list_t;
-
-linked_list_t *linked_list_create(size_t elem_size);
+	size_t (*hash)(void *);
+	float load_factor;
+	size_t entries_count;
+	size_t map_size;
+	void **map_entries;
+	} hashmap_t;
 
 /**
- * return 0 if added, 1 if list or e is null, 2 if failed to alloc a node
+ * allocate a new hashmap
+ * return null on error
  */
-int linked_list_add_head(linked_list_t *list, void *e);
-int linked_list_add_tail(linked_list_t *list, void *e);
+hashmap_t *hashmap_create(size_t initial_cap, float load_factor, size_t (*hash_func)(void*));
 
-iterator_t *linked_list_iterator(linked_list_t *list);
+/**
+ * put an element into the map.
+ * return 0 on success, 1 if failed to grow and 2 if map==NULL || e==NULL
+ */
+int hashmap_add(hashmap_t *map, void *e);
+
+/**
+ * get an element from the map with it's hash.
+ * return NULL on error or if element don't exist
+ */
+void *hashmap_get(hashmap_t *map, size_t hash);
+
+/**
+ * remove element from map.
+ * return 0 on success
+ */
+int hashmap_remove(hashmap_t *map, size_t hash);
+
+/**
+ * free the hashmap.
+ */
+void hashmap_destroy(hashmap_t *map);
+
+iterator_t *hashmap_iterator(hashmap_t *map);
 
 #endif
---
+++ hashmap.c
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * This file is part of libcontainer.
+ *
+ * libcontainer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcontainer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libcontainer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+
+#include <string.h>
+#include <container/hashmap.h>
+#include <stdio.h>
+
+struct hashmap_it
+	{
+	iterator_t it;
+	hashmap_t *map;
+	size_t off;
+	};
+
+hashmap_t *hashmap_create(size_t initial_cap, float load_factor, size_t (*hash_func)(void*))
+	{
+	hashmap_t *hash;
+	if(initial_cap==0 || hash_func==NULL)
+		return NULL;
+
+	hash=malloc(sizeof(hashmap_t));
+	hash->map_entries=malloc(sizeof(void*)*initial_cap);
+	memset(hash->map_entries, 0, sizeof(void*)*initial_cap);
+	hash->map_size=initial_cap;
+	hash->hash=hash_func;
+	hash->load_factor=load_factor;
+	hash->entries_count=0;
+
+	return hash;
+	}
+
+#define index(map, e) (map->hash(e)%map->map_size)
+
+int hashmap_grow(hashmap_t *map)
+	{
+	int i;
+	size_t nsize=map->map_size*2;
+	void **entries=malloc(sizeof(void*)*nsize);
+	if(entries==NULL)
+		return 1;
+	for(i=0; i<map->map_size; i++)
+		{
+		if(map->map_entries[i]!=NULL)
+			{
+			void *e=map->map_entries[i];
+			entries[map->hash(e)%nsize]=e;
+			}
+		}
+	free(map->map_entries);
+	map->map_entries=entries;
+	return 0;
+	}
+
+int hashmap_add(hashmap_t *map, void *e)
+	{
+	size_t i, h;
+	void *e2;
+	if(map==NULL || e==NULL)
+		return 2;
+	
+	if(map->map_size*map->load_factor<=map->entries_count)
+		hasmap_grow(map);
+
+	h=map->hash(e);
+	i=h%map->map_size;
+	e2=map->map_entries[i];
+	while(e2!=NULL && h!=map->hash(e2))
+		{
+		if(++i>map->map_size)
+			i=0;
+		e2=map->map_entries[i];
+		}
+	map->map_entries[i]=e;
+	map->entries_count++;
+	return 0;
+	}
+
+void *hashmap_get(hashmap_t *map, size_t key)
+	{
+	size_t i;
+	void *e;
+	if(map==NULL)
+		return NULL;
+	i=key%map->map_size;
+	e=map->map_entries[i];
+	while(e!=NULL && map->hash(e)!=key)
+		{
+		if(++i>map->map_size)
+			i=0;
+		e=map->map_entries[i];
+		}
+	return e;
+	}
+
+int hashmap_remove(hashmap_t *map, size_t key)
+	{
+	size_t i;
+	void *e;
+	if(map==NULL)
+		return 1;
+	i=key%map->map_size;
+	e=map->map_entries[i];
+	while(e!=NULL && map->hash(e)!=key)
+		{
+		if(++i>map->map_size)
+			i=0;
+		e=map->map_entries[i];
+		}
+	map->map_entries[i]=NULL;
+	map->entries_count--;
+	return 0;
+	}
+
+void hashmap_destroy(hashmap_t *map)
+	{
+	int i;
+	if(map==NULL)
+		return;
+	for(i=0; i<map->map_size; i++)
+		free(map->map_entries[i]);
+	free(map->map_entries);
+	free(map);
+	}
+
+int hashmap_it_has_next(iterator_t *i)
+	{
+	struct hashmap_it *it=(struct hashmap_it *)i;
+	while(it->off<it->map->map_size && it->map->map_entries[it->off]==NULL)
+		it->off++;
+	return it->off<it->map->map_size && it->map->map_entries[it->off]!=NULL;
+	}
+void *hashmap_it_next(iterator_t *i)
+	{
+	struct hashmap_it *it=(struct hashmap_it *)i;
+	return hashmap_it_has_next(i)?it->map->map_entries[it->off++]:NULL;
+	}
+int hashmap_it_reset(iterator_t *i)
+	{
+	struct hashmap_it *it=(struct hashmap_it *)i;
+	it->off=0;
+	return 0;
+	}
+int hashmap_it_remove(iterator_t *i)
+	{
+	struct hashmap_it *it=(struct hashmap_it *)i;
+	free(it->map->map_entries[it->off]);
+	it->map->map_entries[it->off]=NULL;
+	return 0;
+	}
+
+iterator_t *hashmap_iterator(hashmap_t *map)
+	{
+	struct hashmap_it *it=malloc(sizeof(struct hashmap_it));
+
+	it->it.has_next=hashmap_it_has_next;
+	it->it.next=hashmap_it_next;
+	it->it.reset=hashmap_it_reset;
+	it->it.remove=hashmap_it_remove;
+	it->map=map;
+	it->off=0;
+	}
