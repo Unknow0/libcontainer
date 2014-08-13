4c9db4052cf75bb417aefa7104a483240d4291dd
Unknow <unknow0@wanadoo.fr>
2014-08-13 15:38:43 +0200

    moving iterator specialistion in public interface. adding cached global size in element for linked & chuncked list

---
+++ chunked_string.c
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ * This file is part of libcontainer.
+ *
+ * libcontainer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcontainer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libcontainer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+#include <string.h>
+#include <stdlib.h>
+
+#include "container/chunked_string.h"
+
+chunked_string_t *chunked_string_create(size_t chunk_size)
+	{
+	chunked_string_t *list;
+	if(chunk_size==0)
+		return NULL;
+	list=malloc(sizeof(chunked_string_t)+chunk_size);
+	if(list==NULL)
+		return NULL;
+
+	list->chunk_size=chunk_size;
+	list->head=NULL;
+	return list;
+	}
+
+int chunked_string_add(chunked_string_t *list, const char *str)
+	{
+	struct str_chunk *chunk;
+	if(list==NULL || str==NULL)
+		return 2;
+	chunk=list->head;
+	while(chunk!=NULL && chunk->len==list->chunk_size && chunk->next!=NULL)
+		chunk=chunk->next;
+	if(chunk==NULL || chunk->len==list->chunk_size)
+		{
+		struct str_chunk *c=malloc(sizeof(struct str_chunk)+list->chunk_size);
+		if(c==NULL)
+			return 1;
+		c->len=0;
+		c->next=NULL;
+		if(chunk==NULL)
+			list->head=c;
+		else
+			chunk->next=c;
+		chunk=c;
+		}
+
+	unsigned int len=strlen(str);
+	unsigned int l=list->chunk_size-chunk->len;
+	if(l>len)
+		l=len;
+	memmove(chunk->data+chunk->len, str, l);
+	chunk->len+=l;
+	if(l<len)
+		return chunked_string_add(list, str+l);
+	return 0;
+	}
+
+char *chunked_string_get(chunked_string_t *list, unsigned int i)
+	{
+	struct str_chunk *chunk;
+	if(list==NULL)
+		return NULL;
+	chunk=list->head;
+	while(chunk!=NULL && i>=list->chunk_size)
+		{
+		chunk=chunk->next;
+		i-=list->chunk_size;
+		}
+	return chunk==NULL?NULL:chunk->data+i;
+	}
+
+int chunked_string_remove(chunked_string_t *list, unsigned int i, unsigned int len)
+	{
+	struct str_chunk *chunk, *last;
+	if(list==NULL || len==0)
+		return 1;
+	chunk=list->head;
+	while(chunk!=NULL && i>list->chunk_size)
+		{
+		last=chunk;
+		chunk=chunk->next;
+		i-=list->chunk_size;
+		}
+	if(chunk==NULL)
+		return 2;
+	if(chunk->len<i+len)
+		{
+		len-=chunk->len-i;
+		if(i==0)
+			{
+			last->next=chunk->next;
+			free(chunk);
+			chunk=last->next;
+			}
+		else
+			{
+			chunk->len=i;
+			}
+		while(len>chunk->len)
+			{
+			len-=chunk->len;
+			last->next=chunk->next;
+			free(chunk);
+			chunk=last->next;
+			}
+		}
+	if(len>0)
+		{
+		chunk->len-=len;
+		memmove(chunk->data+i, chunk->data+(i+len), chunk->len-i);
+		}
+
+	if(chunk->len==0)
+		{
+		last->next=chunk->next;
+		free(chunk);
+		}
+
+	return 0;
+	}
+
+void chunked_string_destroy(chunked_string_t *list)
+	{
+	struct str_chunk *chunk;
+	if(list==NULL)
+		return;
+	chunk=list->head;
+	while(chunk!=NULL)
+		{
+		struct str_chunk *c=chunk->next;
+		free(chunk);
+		chunk=c;
+		}
+	free(list);
+	}
+
+ void chunked_string_clear(chunked_string_t *list)
+ 	{
+	struct str_chunk *c=list->head;
+	while(c!=NULL)
+		{
+		c->len=0;
+		c=c->next;
+		}
+	}
---
+++ container/chunked_string.h
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * This file is part of libcontainer.
+ *
+ * libcontainer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcontainer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libcontainer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************/
+#ifndef _CHUNKED_STRING_H
+#define _CHUNKED_STRING_H
+
+#include <stdlib.h>
+#include <container/iterator.h>
+
+struct str_chunk
+	{
+	unsigned int len;
+	struct str_chunk *next;
+	char data[0];
+	};
+
+typedef struct chunked_string
+	{
+	size_t chunk_size;
+	struct str_chunk *head;
+	} chunked_string_t;
+
+/**
+ * allocate a new chunked list
+ */
+chunked_string_t *chunked_string_create(size_t chunk_size);
+
+/**
+ * add element to the list, it will be copied in the list.
+ * @return 0 if element is added, 1 if failed to allocate a new chunk, 2 if list or e == NULL.
+ */
+int chunked_string_add(chunked_string_t *list, const char *str);
+
+/**
+ * retrun the nth element or NULL if i > list size or list == NULL.
+ * /!\ you got access directly on the list do no free returned element!
+ */
+char *chunked_string_get(chunked_string_t *list, unsigned int i);
+
+/**
+ * remove and return element from the list.
+ * return NULL if list == NULL or i > list_size
+ * /!\ you should free the returned element.
+ */
+//char *chunked_string_remove_return(chunked_string_t *list, unsigned int i, unsigned int len);
+
+/** return 0 on success */
+int chunked_string_remove(chunked_string_t *list, unsigned int i, unsigned int len);
+
+/**
+ * free the list and all element returned by chunked_string_get
+ */
+void chunked_string_destroy(chunked_string_t *list);
+
+/**
+ * remove all data from this list.
+ * /!\ dont remove unused chunk.
+ */
+ void chunked_string_clear(chunked_string_t *list);
+
+#endif
