92df830cbf7b41b4a09ce1a13467264cb227c661
Unknow <unknow0@free.fr>
2015-11-25 18:43:52 +0100

    fix header

--- includes/utils/watch.h
+++ includes/utils/watch.h
@@ -1,18 +1,18 @@
 /*******************************************************************************
- * This file is part of mserver.
+ * This file is part of utils.
  *
- * mserver is free software; you can redistribute it and/or modify
+ * utils is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * mserver is distributed in the hope that it will be useful,
+ * utils is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License along
- * with mserver; if not, write to the Free Software Foundation, Inc.,
+ * with utils; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ******************************************************************************/
 #ifndef _WATCH_H
